fccc14222d578fa414156d2a761dbec29f0ffa69
abhishekanimatron <abhishekverma297ab@gmail.com>
2023-12-15 23:13:53 +0530

    sign up page's UI created

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { FaSearch } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 export default function Header() {
     return (
-        <header className='bg-slate-200 shadow-md'>
+        <header className='bg-violet-200 shadow-md'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
                 <Link to='/'>
                     <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input type="text" placeholder='Username' className='border p-3 rounded-full' id='username' />
+        <input type="email" placeholder='Email' className='border p-3 rounded-full' id='email' />
+        <input type="password" placeholder='Password' className='border p-3 rounded-full' id='password' />
+        <button className="text-white bg-purple-800 p-3 rounded-full uppercase hover:opacity-95">Sign Up</button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className='text-purple-700'>Sign in</span>
+        </Link>
+      </div>
+    </div>
   )
 }
