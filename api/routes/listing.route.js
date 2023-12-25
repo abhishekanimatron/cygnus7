516ad5d363d56127cf514c78229cb8897c58a729
abhishekanimatron <abhishekverma297ab@gmail.com>
2023-12-26 00:08:29 +0530

    update listing ui works

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createListing, updateListing, deleteListing } from '../controllers/listing.controller.js'
+import { createListing, getListing, updateListing, deleteListing } from '../controllers/listing.controller.js'
 import { verifyToken } from '../utils/verifyUser.js'
 
 const router = express.Router()
@@ -7,5 +7,6 @@ const router = express.Router()
 router.post('/create', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
 router.post('/update/:id', verifyToken, updateListing)
+router.get('/get/:id', getListing)
 
 export default router
