4f2b795d7381b92f14353a5fcaf315fe3bd3dee6
abhishekanimatron <abhishekverma297ab@gmail.com>
2024-01-04 10:27:13 +0530

    name update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
                 <Link to='/'>
                     <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-                        <span className='text-purple-600'>Hopeless</span>
+                        <span className='text-purple-600'>Cygnus</span>
                         <span className='text-purple-800'>Estate</span>
                     </h1>
                 </Link>
